142693e6a9d2f4dbc4d366c6c801340460b747ff
Asim-Shahriar <asimshariar@gmail.com>
2020-05-28 23:32:58 +0600

    CountryDescriptionFragment added

--- app/src/main/java/com/example/fragmentcallback/CountryDescriptionFragment.java
+++ app/src/main/java/com/example/fragmentcallback/CountryDescriptionFragment.java
@@ -2,63 +2,77 @@ package com.example.fragmentcallback;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CountryDescriptionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CountryDescriptionFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CountryDescriptionFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CountryDescriptionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CountryDescriptionFragment newInstance(String param1, String param2) {
-        CountryDescriptionFragment fragment = new CountryDescriptionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private static final String COMMON_TAG="CombinedLifeCycle";
+    private static final String FRAGMENT_NAME=CountryDescriptionFragment.class.getSimpleName();
+    private static final String TAG=COMMON_TAG;
+    View rootView;
+    TextView textViewCountryDescription;
+    String countryName;
+    String countryDescription;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_country_description, container, false);
+        rootView= inflater.inflate(R.layout.fragment_country_description, container, false);
+        initUI();
+        return rootView;
+    }
+
+    private void initUI() {
+        textViewCountryDescription = (TextView)rootView.findViewById(R.id.textViewCountryDescription);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Bundle bundle=getArguments();
+        countryName=bundle.getString(FragmentActionListener.KEY_SELECTED_COUNTRY,"India");
+        countryDescription=getString(getStringId(countryName));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(countryName);
+        textViewCountryDescription.setText(countryDescription);
+    }
+
+    private int getStringId(String countryName) {
+
+        if(countryName.equals("India")){
+            return R.string.India;
+        }else if(countryName.equals("USA")){
+            return R.string.USA;
+        }else if(countryName.equals("Pakistan")){
+            return R.string.Pakistan;
+        }else if(countryName.equals("Bangladesh")){
+            return R.string.Bangladesh;
+        }else if(countryName.equals("Egypt")){
+            return R.string.Egypt;
+        }else if(countryName.equals("Indonesia")){
+            return R.string.Indonesia;
+        }else if(countryName.equals("UK")){
+            return R.string.UK;
+        }else if(countryName.equals("Germany")){
+            return R.string.Germany;
+        }else {
+            return R.string.India;
+        }
+    }
+    public String toString(){
+        return this.getClass().getSimpleName();
     }
 }
